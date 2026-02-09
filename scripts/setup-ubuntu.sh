23e819c17cdae201155cc67016d5127ff980d1a4
Max Tropets <maxtropets@gmail.com>
2026-02-09 13:16:56 +0000

    Try tests

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -4,5 +4,5 @@ set -ex
 apt-get update
 apt-get install -y build-essential
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --component rustfmt
 export PATH="$HOME/.cargo/bin:$PATH"
